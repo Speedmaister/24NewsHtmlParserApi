8a0284cb66e698b49fcd6c157760062abf256786
Speedmaister <radosholev@gmail.com>
2013-10-12 11:18:20 +0300

    asd

---
+++ HtmlParser/Controllers/WeatherController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace HtmlParser.Controllers
+{
+    public class WeatherController : ApiController
+    {
+        [HttpGet]
+        [ActionName("get")]
+        public string GetWeather(string url)
+        {
+            var client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            var json = client.DownloadString(url);
+            return json;
+        }
+    }
+}
